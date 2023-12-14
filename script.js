16b1c339cc233b68a4d7f276218dc0eb6aa4c150
maybejenna <149430949+maybejenna@users.noreply.github.com>
2023-12-14 11:44:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions');
+
+const fruitKeyWords = [
+    'Apple', 
+    'Apricot',
+    'Avocado 🥑', 
+    'Banana', 
+    'Bilberry', 
+    'Blackberry', 
+    'Blackcurrant', 
+    'Blueberry', 
+    'Boysenberry', 
+    'Currant', 
+    'Cherry', 
+    'Coconut', 
+    'Cranberry', 
+    'Cucumber', 
+    'Custard apple', 
+    'Damson', 
+    'Date', 
+    'Dragonfruit', 
+    'Durian', 
+    'Elderberry', 
+    'Feijoa', 
+    'Fig', 
+    'Gooseberry', 
+    'Grape', 
+    'Raisin', 
+    'Grapefruit', 
+    'Guava', 
+    'Honeyberry', 
+    'Huckleberry', 
+    'Jabuticaba', 
+    'Jackfruit', 
+    'Jambul', 
+    'Juniper berry', 
+    'Kiwifruit', 
+    'Kumquat', 
+    'Lemon', 
+    'Lime', 
+    'Loquat', 
+    'Longan', 
+    'Lychee', 
+    'Mango', 
+    'Mangosteen', 
+    'Marionberry', 
+    'Melon', 
+    'Cantaloupe', 
+    'Honeydew', 
+    'Watermelon', 
+    'Miracle fruit', 
+    'Mulberry', 
+    'Nectarine', 
+    'Nance', 
+    'Olive', 
+    'Orange', 
+    'Clementine', 
+    'Mandarine', 
+    'Tangerine', 
+    'Papaya', 
+    'Passionfruit', 
+    'Peach', 
+    'Pear', 
+    'Persimmon', 
+    'Plantain', 
+    'Plum', 
+    'Pineapple', 
+    'Pomegranate', 
+    'Pomelo', 
+    'Quince', 
+    'Raspberry', 
+    'Salmonberry', 
+    'Rambutan', 
+    'Redcurrant', 
+    'Salak', 
+    'Satsuma', 
+    'Soursop', 
+    'Star fruit', 
+    'Strawberry', 
+    'Tamarillo', 
+    'Tamarind', 
+    'Yuzu'
+];
+
+let search = input.onkeyup = function(){
+	let results = [];
+	let inputResult = input.value;
+		if(inputResult.length){
+			results = fruitKeyWords.filter((keyword)=>{
+				return keyword.toLowerCase().includes(inputResult.toLowerCase()); 
+			}); 
+			// console.log(results)
+		}
+		showSuggestions(results);
+}
+
+
+function searchHandler(e) {
+	// TODO
+}
+
+function showSuggestions(results, inputVal) {
+	const content = results.map((list)=>{
+        return `<li onclick="useSuggestion(event)">${list}</li>`;
+    });
+
+	suggestions.innerHTML = "<ul>"+ content.join("") +"</ul>"
+}
+
+function useSuggestion(e) {
+    const clickedElement = e.target;
+    input.value = clickedElement.textContent || clickedElement.innerText;
+	suggestions.innerHTML = ``;
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
